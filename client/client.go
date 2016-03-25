0886da279789dfc6d2c73144c4ab62bfe8df521a
Christian Kellner <christian@kellner.me>
2016-03-25 16:58:28 +0100

    [client] RepoAccess added

--- client/client.go
+++ client/client.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 	"net/url"
 
+	"bytes"
 	"encoding/json"
 	"fmt"
 	. "github.com/G-Node/gin-repo/common"
+	"github.com/G-Node/gin-repo/wire"
 	"io/ioutil"
 )
 
@@ -45,4 +47,33 @@ func (client *Client) LookupUserByFingerprint(fingerprint string) (*User, error)
 	}
 
 	return &user, nil
+}
+
+func (client *Client) RepoAccess(path string, uid string, method string) (string, error) {
+	web := &http.Client{}
+
+	query := wire.RepoAccessQuery{Path: path, User: uid, Method: method}
+
+	data, err := json.Marshal(&query)
+
+	if err != nil {
+		return "", err
+	}
+
+	url := fmt.Sprintf("%s/intern/repos/access", client.address)
+	res, err := web.Post(url, "application/json", bytes.NewReader(data))
+	if err != nil {
+		return "", err
+	} else if status := res.StatusCode; status != 200 {
+		return "", fmt.Errorf("Server returned non-OK status: %d", status)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
