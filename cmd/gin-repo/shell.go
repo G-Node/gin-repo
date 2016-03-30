79556a4838ab1259f915a1840e700061c1b1aa16
Christian Kellner <christian@kellner.me>
2016-03-30 15:19:48 +0200

    [cmd/shell] git-annex-shell support

--- cmd/gin-repo/shell.go
+++ cmd/gin-repo/shell.go
@@ -8,9 +8,11 @@ import (
 	"syscall"
 
 	"github.com/G-Node/gin-repo/client"
+	"github.com/G-Node/gin-repo/git"
 )
 
 func execGitCommand(name string, args ...string) int {
+	// fmt.Fprintf(os.Stderr, "[D] ! %s, %#v", name, args)
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -50,6 +52,52 @@ func gitCommand(args []string, push bool, uid string) int {
 	return execGitCommand(args[0], path)
 }
 
+func gitAnnex(args []string, uid string) int {
+
+	if len(args) < 3 {
+		fmt.Fprintf(os.Stderr, "ERROR: wrong arguments to %q", args[0])
+		return -2
+	}
+
+	client := client.NewClient("http://localhost:8888")
+	path, pok, err := client.RepoAccess(args[2], uid)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[E] repo access error: %v\n", err)
+		return -10
+	}
+
+	repo, err := git.OpenRepository(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Could not open repository.")
+		return -15
+	}
+
+	if pok && !repo.HasAnnex() {
+		err = repo.InitAnnex()
+		if err != nil {
+			//TODO: don't print the internal error, might give away the actual path
+			fmt.Fprintf(os.Stderr, "ERROR: initialization of git-annex failed: %v", err)
+			return -14
+		}
+	}
+
+	args[2] = path
+
+	// "If set, disallows running git-shell to handle unknown commands."
+	os.Setenv("GIT_ANNEX_SHELL_LIMITED", "True")
+
+	// "If set, git-annex-shell will refuse to run commands
+	//  that do not operate on the specified directory."
+	os.Setenv("GIT_ANNEX_SHELL_DIRECTORY", path)
+
+	if !pok {
+		os.Setenv("GIT_ANNEX_SHELL_READONLY", "True")
+	}
+
+	return execGitCommand(args[0], args[1:]...)
+}
+
 func cmdShell(args map[string]interface{}) {
 	log.SetOutput(os.Stderr)
 
@@ -78,6 +126,9 @@ func cmdShell(args map[string]interface{}) {
 
 	case "git-receive-pack":
 		res = gitCommand(argv, true, uid)
+
+	case "git-annex-shell":
+		res = gitAnnex(argv, uid)
 
 	default:
 		fmt.Fprintf(os.Stderr, "[E] unhandled command: %s\n", cmd)
