12ddfd3dbaef277234ad7455cc5c0bd350955bfc
Christian Kellner <christian@kellner.me>
2016-04-01 21:19:30 +0200

    [server] add Server struct and log requests
    Also recover from panics

--- cmd/gin-repod/main.go
+++ cmd/gin-repod/main.go
@@ -2,13 +2,69 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/docopt/docopt-go"
 
 	"github.com/gorilla/mux"
+	"os"
+	"runtime"
 )
+
+type Server struct {
+	http.Server
+
+	Root *mux.Router
+}
+
+type LogLevel int
+
+const (
+	PANIC LogLevel = iota
+	ERROR
+	WARN
+	INFO
+	DEBUG
+)
+
+func (s *Server) log(lvl LogLevel, msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			buf := make([]byte, 1024*10)
+			n := runtime.Stack(buf, false)
+			st := string(buf[:n])
+
+			s.log(ERROR, "'%s %s' 500, Recovered from panic!", req.Method, req.URL.Path)
+			fmt.Fprintf(os.Stderr, "Panic on reqest: '%s %s': %q", req.Method, req.URL.Path, st)
+		} else {
+			s.log(INFO, "'%s %s'", req.Method, req.URL.Path)
+		}
+	}()
+
+	s.Root.ServeHTTP(w, req)
+}
+
+func (s *Server) ListenAndServe() error {
+	s.log(INFO, "Listening on %s", s.Addr)
+	err := s.Server.ListenAndServe()
+	if err != nil {
+		s.log(ERROR, "ListenAndServe() error: %v", err)
+	}
+	return err
+}
+
+func NewServer(addr string) *Server {
+	s := &Server{Server: http.Server{Addr: addr}, Root: mux.NewRouter()}
+	s.Handler = s
+	return s
+}
 
 func main() {
 	usage := `gin repo daemon.
@@ -25,15 +81,13 @@ Options:
 	args, _ := docopt.Parse(usage, nil, true, "gin repod 0.1a", false)
 	fmt.Println(args)
 
-	r := mux.NewRouter()
+	s := NewServer(":8888")
+	r := s.Root
+
 	r.HandleFunc("/intern/user/lookup", lookupUser).Methods("GET")
 	r.HandleFunc("/intern/repos/access", repoAccess).Methods("POST")
 
 	r.HandleFunc("/users/{user}/repos", createRepo).Methods("POST")
 
-	http.Handle("/", r)
-
-	hostport := ":8888"
-	log.Printf("Listening on " + hostport)
-	log.Fatal(http.ListenAndServe(hostport, nil))
+	s.ListenAndServe()
 }
