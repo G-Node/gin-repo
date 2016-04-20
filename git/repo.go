640d1157c600c22b2eb123df5390fa1f20836f7a
Christian Kellner <christian@kellner.me>
2016-04-20 21:27:01 +0200

    [git] parse annex keys ourselves

--- git/repo.go
+++ git/repo.go
@@ -1,13 +1,17 @@
 package git
 
 import (
-	"encoding/json"
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Repository struct {
@@ -198,35 +202,90 @@ func (repo *Repository) Readlink(id SHA1) (string, error) {
 	return string(data), nil
 }
 
-//AnnexKeyInfo corresponds to the output for git annex examinekey
-//see Repository.AnnexExamineKey
-type AnnexKeyInfo struct {
-	Key          string
-	Backend      string
-	Bytesize     string // annex returns this as string in json
-	Humansize    string
-	Keyname      string
-	Hashdirlower string
-	Hashdirmixed string
-	MTime        string
+//AnnexKey represents an annex key. Key, Backend and Keyname
+// fields are guaranteed to be there. Presence of other fields
+// depends on the used backend.
+type AnnexKey struct {
+	Key      string
+	Backend  string
+	Bytesize int64
+	Keyname  string
+	MTime    *time.Time
+
+	hash [16]byte
 }
 
-func (repo *Repository) AnnexExamineKey(name string) (AnnexKeyInfo, error) {
-	gdir := fmt.Sprintf("--git-dir=%s", repo.Path)
-	cmd := exec.Command("git", gdir, "annex", "examinekey", name, "--json")
-	body, err := cmd.Output()
+//HashDirLower is the new key hash format. It uses two directories,
+// each dir name consisting of three lowercase letters.
+// (c.f. http://git-annex.branchable.com/internals/hashing/)
+func (key *AnnexKey) HashDirLower() string {
+	hs := hex.EncodeToString(key.hash[:])
+	return string(hs[:3]) + string(os.PathSeparator) +
+		string(hs[3:6]) + string(os.PathSeparator)
+}
 
-	var info AnnexKeyInfo
-	if err != nil {
-		return info, err
+const (
+	annexKeyChars = "0123456789zqjxkmvwgpfZQJXKMVWGPF"
+)
+
+//HashDirMixed is the old key hash format. It uses two directories,
+// each dir name consisting of two letters.
+// (c.f. http://git-annex.branchable.com/internals/hashing/)
+func (key *AnnexKey) HashDirMixed() string {
+	var l [4]string
+	w := binary.LittleEndian.Uint32(key.hash[:4])
+	for i := uint(0); i < 4; i++ {
+		l[i] = string(annexKeyChars[int(w>>(6*i)&0x1F)])
+	}
+	return l[1] + l[0] + string(os.PathSeparator) +
+		l[3] + l[2] + string(os.PathSeparator)
+}
+
+//AnnexExamineKey parses the key and extracts all available information
+//from the key string. See AnnexKey for more details.
+//(cf. http://git-annex.branchable.com/internals/key_format/)
+func AnnexExamineKey(keystr string) (*AnnexKey, error) {
+
+	key := AnnexKey{Key: keystr, hash: md5.Sum([]byte(keystr))}
+
+	front, name := split2(keystr, "--")
+	key.Keyname = name
+
+	parts := strings.Split(front, "-")
+
+	if len(parts) < 1 {
+		// key error
+		return nil, fmt.Errorf("git: bad annex key (need backend--name)")
 	}
 
-	err = json.Unmarshal(body, &info)
-	if err != nil {
-		return info, err
+	key.Backend = parts[0]
+	for i := 1; i < len(parts); i++ {
+		part := parts[i]
+
+		if len(part) < 1 {
+			continue
+		}
+
+		k, v := part[0], part[1:]
+
+		switch k {
+		case 's':
+			i, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				continue
+			}
+			key.Bytesize = i
+		case 'm':
+			i, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				continue
+			}
+			t := time.Unix(i, 0)
+			key.MTime = &t
+		}
 	}
 
-	return info, nil
+	return &key, nil
 }
 
 //IsAnnexFile returns true if the file at path is
@@ -245,14 +304,14 @@ type AnnexStat struct {
 func (repo *Repository) Astat(target string) (*AnnexStat, error) {
 
 	sbuf := AnnexStat{Name: filepath.Base(target)}
-	ki, err := repo.AnnexExamineKey(sbuf.Name)
+	ki, err := AnnexExamineKey(sbuf.Name)
 
 	if err != nil {
 		return nil, err
 	}
 
 	// we are in a bare repository, therefore we use hasdirlower
-	p := filepath.Join(repo.Path, "annex", "objects", ki.Hashdirlower, ki.Key, ki.Key)
+	p := filepath.Join(repo.Path, "annex", "objects", ki.HashDirLower(), ki.Key, ki.Key)
 	fi, err := os.Stat(p)
 
 	if err != nil {
