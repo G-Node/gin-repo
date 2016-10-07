3dfb1be0e6105e0f9f6d0ef8c8208d2f38af33fc
Christian Kellner <christian@kellner.me>
2016-10-07 16:51:35 +0200

    [contrib] main.sh: generate test data if requested
    Enable by setting GRD_GENDATA

--- contrib/main.sh
+++ contrib/main.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-#Ensure we have the right permissions on /data
-find . -perm 0444 -exec chmod 0666 {} \; -exec chown git:git {} \; -exec chmod 0444 {} \;
-chown -Rf git:git /data
+echo "Starting gin-repo service"
+echo " GRD_GENDATA=${GRD_GENDATA}"
+
+if [ ! -z ${GRD_GENDATA+x} ]; then
+    git config --global user.name "gin repo docker"
+    git config --global user.email "gin-repo@g-node.org"
+    DATA=${GRD_DATAFILE:-$GOPATH/src/github.com/G-Node/gin-repo/contrib/data.yml}
+    echo "Generating data [$DATA]"
+    $GOPATH/src/github.com/G-Node/gin-repo/contrib/mkdata.py $DATA
+fi
+
 supervisord -c/etc/supervisord.conf
