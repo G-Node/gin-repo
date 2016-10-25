42e659983e682b8a1c06b186b53a98e79dc236c1
M. Sonntag <michael.p.sonntag@gmail.com>
2016-10-25 21:57:14 +0200

    [store] add TestMain

--- store/repo_test.go
+++ store/repo_test.go
@@ -1,9 +1,16 @@
 package store
 
 import (
+	"fmt"
+	"os"
 	"reflect"
 	"testing"
+
+	"github.com/G-Node/gin-repo/internal/testbed"
 )
+
+var users UserStore
+var repos *RepoStore
 
 var defaultRepo = &RepoId{"foo", "bar"}
 
@@ -55,4 +62,46 @@ func TestParseRepoId(t *testing.T) {
 			t.Errorf("RepoIdParse(%q) => %v, want %v", tt.in, out, *tt.out)
 		}
 	}
+}
+
+// TestMain sets up a temporary user store for store method tests.
+// Currently the temporary files created by this function are not cleaned up.
+func TestMain(m *testing.M) {
+	repoDir, err := testbed.MkData("/tmp")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not make test data: %v\n", err)
+		os.Exit(1)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get cwd: %v", err)
+		os.Exit(1)
+	}
+
+	// Change to repoDir is required, since creating a local user store depends on
+	// auth.ReadSharedSecret(), which reads a file "gin.secret" from the current directory
+	// and fails otherwise.
+	err = os.Chdir(repoDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not set cwd to repoDir: %v", err)
+		os.Exit(1)
+	}
+
+	users, err = NewUserStore(repoDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not create local user store: %v\n", err)
+		os.Exit(1)
+	}
+
+	repos, err = NewRepoStore(repoDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not create local repo store: %v\n", err)
+		os.Exit(1)
+	}
+
+	res := m.Run()
+
+	_ = os.Chdir(cwd)
+	os.Exit(res)
 }
